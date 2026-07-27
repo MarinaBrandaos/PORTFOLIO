5632e452b918a7dd2293e61c981e0ce51ba7ac89
Marina <marinabs496@gmail.com>
2026-07-26 21:14:36 -0300

    css

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,6 +1,6 @@
 
     <nav>
-         <a href="{{route('admin')}}"><img src="{{asset('storage/images/logo.png')}}" alt="Logo"></img></a>
+         <a href="{{route('admin')}}"><img src="{{asset('images/logo.png')}}" alt="Logo"></img></a>
         <ul>
             <li><a href="{{route('index')}}">Home</a></li>
             <li><a href="{{route('about')}}">About</a></li>
