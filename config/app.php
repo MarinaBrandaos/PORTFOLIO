23a3386016083e34fcdeb78c4ae1c9dc35dc7705
Marina <marinabs496@gmail.com>
2026-07-29 14:41:49 -0300

    url

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Portifolio-marina'),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://marina-brandao-silva-portfolio.up.railway.app'),
 
     /*
     |--------------------------------------------------------------------------
