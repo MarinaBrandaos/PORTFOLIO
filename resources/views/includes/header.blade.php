85c211e975d6b80912b416017d497545d0cc20cc
Marina <marinabs496@gmail.com>
2026-08-06 23:15:42 -0300

    alterações cta 2

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -6,7 +6,4 @@
             <li><a href="{{route('about')}}">About</a></li>
         </ul>
     </nav>
-    <a href="https://wa.me/5575991150851?text=Olá! Vi seu portfólio e quero saber mais sobre um orçamento" 
-   class="whatsapp-float" target="_blank" aria-label="Falar no WhatsApp">
-    <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/whatsapp.svg" alt="WhatsApp" style="width: 28px; filter: invert(1);">
-</a>
+
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -12,5 +12,9 @@
         <footer>
             @include('includes.footer')
         </footer>
+            <a href="https://wa.me/5575991150851?text=Olá! Vi seu portfólio e quero saber mais sobre um orçamento" 
+   class="whatsapp-float" target="_blank" aria-label="Falar no WhatsApp">
+    <img src="https://cdn.jsdelivr.net/gh/simple-icons/simple-icons/icons/whatsapp.svg" alt="WhatsApp" style="width: 28px; filter: invert(1);">
+</a>
     </body>
 </html>
